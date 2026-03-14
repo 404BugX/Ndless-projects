c27da8f2bff6af80c7636661922f8f0af7e3b0e6
404BugX <167647750+404BugX@users.noreply.github.com>
2026-03-13 19:30:41 -0700

    Add files via upload

---
+++ HelloWorld/main.c
@@ -0,0 +1,11 @@
+#include <libndls.h>
+#include <os.h>
+
+int main(void) {
+    // Learn_HelloWorld
+    // Pressez une touche pour quitter
+
+    show_msgbox("TESTBOX", "Hello World!");
+
+    return 0;
+}
